5c0837c6c9c39459e40c62081fe7db4f7b246bef
Saular Raffi <saular.raffi@yahoo.com>
2023-07-29 18:14:43 -0400

    create top app bar with Add button the right

--- src/components/TopAppBar/index.js
+++ src/components/TopAppBar/index.js
@@ -1,14 +1,31 @@
 import { React, useEffect, useState } from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import AddBoxIcon from '@mui/icons-material/AddBox';
 
 const TopAppBar = () => {
     useEffect(() => {
-
+        
     }, [])
 
     return (
-        <>
-        
-        </>
+        <Box sx={{ flexGrow: 1, marginBottom: "50px" }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2, marginLeft: "auto" }}
+                    >
+                        <AddBoxIcon />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </Box>
     )
 }
 
