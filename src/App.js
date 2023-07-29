dabdc31deea368809d348c1f691ccaf6a69f75d3
Saular Raffi <saular.raffi@yahoo.com>
2023-07-29 17:49:28 -0400

    migrate from @material-ui/core to @mui/material

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Test from './components/test'
 import GifNotesView from './components/GifNotesView';
+import TopAppBar from './components/TopAppBar';
 import './App.css';
 
 function App() {
   return (
     <>
       {/* <Test /> */}
+      <TopAppBar />
       <GifNotesView />
     </>
   );
--- src/components/GifNotesView/GifNoteTile/index.js
+++ src/components/GifNotesView/GifNoteTile/index.js
@@ -1,5 +1,5 @@
 import { React } from 'react'
-import { Container, Typography, Paper } from '@material-ui/core'
+import { Container, Typography, Paper } from '@mui/material'
 
 const GifNoteTile = ({note, gifUrl}) => {
     const styles = {
