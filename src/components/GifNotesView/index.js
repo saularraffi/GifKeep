758278e6a6ff58da63e0f7bade92bf6252ec9dbb
Saular Raffi <saular.raffi@yahoo.com>
2023-07-28 22:18:53 -0400

    implement material grid, still need to align horizontally

--- src/components/GifNotesView/index.js
+++ src/components/GifNotesView/index.js
@@ -1,6 +1,8 @@
-import { React, useEffect, useRef, useState } from 'react'
+import { React, useEffect, useState } from 'react'
 import { getGifNotes } from '../../services/gifyuApi'
 import GifNoteTile from './GifNoteTile'
+import { Box, Grid } from '@material-ui/core'
+
 
 const GifNoteView = () => {
     const [gifNotes, setGifNotes] = useState([]);
@@ -12,16 +14,22 @@ const GifNoteView = () => {
     }, [])
 
     return (
-        <>
-            {gifNotes.map((gifNote) => { 
-                return <GifNoteTile
-                    key={gifNote._id}
-                    note={gifNote.note} 
-                    category={gifNote.category} 
-                    gifUrl={gifNote.gifUrl}
-                />
-            })}
-        </>
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                {gifNotes.map((gifNote) => { 
+                    return (
+                        <Grid item xs={8}>
+                            <GifNoteTile
+                                key={gifNote._id}
+                                note={gifNote.note} 
+                                category={gifNote.category} 
+                                gifUrl={gifNote.gifUrl}
+                            />
+                        </Grid>
+                    )
+                })}
+            </Grid>
+        </Box>
     )
 }
 
