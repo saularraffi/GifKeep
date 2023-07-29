e97b431649cef069965976a396d8d95542201b99
Saular Raffi <saular.raffi@yahoo.com>
2023-07-29 15:27:14 -0400

    refine tile design

--- src/components/GifNotesView/GifNoteTile/index.js
+++ src/components/GifNotesView/GifNoteTile/index.js
@@ -1,36 +1,26 @@
 import { React } from 'react'
-import { Container, Typography, Divider, Paper } from '@material-ui/core'
+import { Container, Typography, Paper } from '@material-ui/core'
 
 const GifNoteTile = ({note, gifUrl}) => {
     const styles = {
         root: {
             backgroundColor: "white",
-            padding: "20px 0 0 0",
             border: "1px solid rgba(0, 0, 0, 0.12)"
         },
         img: {
             width: "100%",
-            borderRadius: "5px"
         },
         typography: {
             fontFamily: "Bree Serif",
             fontSize: "20px",
             paddingTop: "15px",
             paddingBottom: "15px"
-        },
-        divider: {
-            marginTop: "15px",
-            width: "100%",
-            borderBottomWidth: "20px"
         }
     }
 
     return (
         <Paper style={styles.root} elevation={2}>
-            <Container>
-                <img src={gifUrl} alt="" style={styles.img}/>
-            </Container>
-            <Divider style={styles.divider} />
+            <img src={gifUrl} alt="" style={styles.img}/>
             <Container>
                 <Typography style={styles.typography}>{note}</Typography>
             </Container>
