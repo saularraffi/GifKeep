d3de7e517a062e93c369d35af6253c979560cfd5
Saular Raffi <saular.raffi@yahoo.com>
2023-07-30 15:27:15 -0400

    reize Add button, change app bar color, add GifKeep title

--- src/components/TopAppBar/index.js
+++ src/components/TopAppBar/index.js
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import AddBoxIcon from '@mui/icons-material/AddBox';
+import { Typography } from '@mui/material';
 
 const TopAppBar = () => {
     useEffect(() => {
@@ -12,8 +13,9 @@ const TopAppBar = () => {
 
     return (
         <Box sx={{ flexGrow: 1, marginBottom: "50px" }}>
-            <AppBar position="static">
+            <AppBar position="static" sx={{ backgroundColor: "#3a3e4d" }}>
                 <Toolbar>
+                    <Typography sx={{ fontFamily: "Bree Serif", fontSize: "2rem" }}>GifKeep</Typography>
                     <IconButton
                         size="large"
                         edge="start"
@@ -21,7 +23,7 @@ const TopAppBar = () => {
                         aria-label="menu"
                         sx={{ mr: 2, marginLeft: "auto" }}
                     >
-                        <AddBoxIcon />
+                        <AddBoxIcon sx={{ fontSize: "2.5rem" }} />
                     </IconButton>
                 </Toolbar>
             </AppBar>
