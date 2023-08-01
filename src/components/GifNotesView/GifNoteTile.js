74d3f7940125d8ea63a3992166954bc7c9912d3e
Saular Raffi <saular.raffi@yahoo.com>
2023-08-01 12:06:23 -0400

    add menu dropdown to each tile, add delete btn to menu

--- src/components/GifNotesView/GifNoteTile.js
+++ src/components/GifNotesView/GifNoteTile.js
@@ -1,25 +1,66 @@
-import { React } from 'react'
-import { Container, Typography, Paper } from '@mui/material'
+import { React, useState } from 'react'
+import { Container, Typography, Paper, IconButton, Box, Grid } from '@mui/material'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const styles = {
     root: {
         backgroundColor: "white",
         border: "1px solid rgba(0, 0, 0, 0.12)"
     },
-    img: {
-        width: "100%",
-    },
     typography: {
         fontSize: "20px",
-        paddingTop: "15px",
-        paddingBottom: "15px"
+        paddingTop: "5px",
+        paddingBottom: "10px"
     }
 }
 
 const GifNoteTile = ({note, gifUrl}) => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleDelete = () => {
+        handleClose();
+    };
+
+    const MenuOptions = () => {
+        return (
+            <>
+                <MenuItem onClick={handleDelete}>
+                    <Typography sx={{ fontFamily: "Kanit", color: "red" }}>DELETE</Typography>
+                </MenuItem>       
+            </>
+        )
+    };
+
     return (
         <Paper style={styles.root} elevation={2}>
-            <img src={gifUrl} alt="" style={styles.img}/>
+            <Box sx={{ float: "right" }}>
+                <IconButton onClick={handleClick}>
+                    <MoreVertIcon />
+                </IconButton>
+                <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                        'aria-labelledby': 'basic-button',
+                    }}
+                >
+                    <MenuOptions />
+                </Menu>
+            </Box>
+            <img src={gifUrl} alt="" style={{ width: "100%" }}/>
             <Container>
                 <Typography style={styles.typography}>{note}</Typography>
             </Container>
