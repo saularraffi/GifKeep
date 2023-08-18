9024285a4651fbe66cb864357ba6bda70748a25a
Saular Raffi <saular.raffi@yahoo.com>
2023-08-18 19:19:29 -0400

    prettier

--- server/app.js
+++ server/app.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const path = require('path');
-const cors = require('cors')
-const config = require('./config')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cors = require("cors");
+const config = require("./config");
 
 // requiring routes
 const gifNotesController = require("./api/controllers/gifNotesController");
 const usersController = require("./api/controllers/userController");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
@@ -21,15 +21,18 @@ app.use(bodyParser.json());
 app.use(gifNotesController);
 app.use(usersController);
 
-mongoose.connect(config.db.connectionString, config.db.options)
-.then(() => {
-    console.log("\n[+] MongoDB connection successful");
-    
-    app.listen(config.app.port, config.app.host, () => {
-        console.log(`\n[+] Server running at http://${config.app.host}:${config.app.port}/`);
-    });    
-})
-.catch((err) => {
-    console.log("\n[-] MongoDB connection failed\n");
-    // console.log(err)
-})
+mongoose
+    .connect(config.db.connectionString, config.db.options)
+    .then(() => {
+        console.log("\n[+] MongoDB connection successful");
+
+        app.listen(config.app.port, config.app.host, () => {
+            console.log(
+                `\n[+] Server running at http://${config.app.host}:${config.app.port}/`
+            );
+        });
+    })
+    .catch((err) => {
+        console.log("\n[-] MongoDB connection failed\n");
+        // console.log(err)
+    });
