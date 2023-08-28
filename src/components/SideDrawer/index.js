c3fd6bb63c09ea408aa8a5fba179edb63b36a793
Saular Raffi <saular.raffi@yahoo.com>
2023-08-28 11:32:29 -0400

    stream videos to client, create GET endpoint for fetching thumbnails, intially load video thumbnails and switch to video when use clicks

--- src/components/SideDrawer/index.js
+++ src/components/SideDrawer/index.js
@@ -1,36 +1,42 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
-import IconButton from '@mui/material/IconButton';
-import AddBoxIcon from '@mui/icons-material/AddBox';
-import TextField from '@mui/material/TextField';
-import theme from '../../theme';
-import { putUser } from '../../services/usersApi';
-import CategoryRow from './CategoryRow';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import TextField from "@mui/material/TextField";
+import theme from "../../theme";
+import { putUser } from "../../services/usersApi";
+import CategoryRow from "./CategoryRow";
 
 const styles = {
     dividerContainer: {
         display: "flex",
         alignItems: "center",
-        width: "35%"
+        width: "35%",
     },
     divider: {
         width: "100%",
-        borderBottomWidth: "2px"
-    }
-}
+        borderBottomWidth: "2px",
+    },
+};
 
 export default function SideDrawer({ open, setOpen, setSharedCategoryState }) {
-    const lightGrey = "#c7c7c7"
-    const [addCategoryButtonColor, setAddCategoryButtonColor] = React.useState(lightGrey)
-    const [categories, setCategories] = React.useState(localStorage.getItem("categories").split(","));
+    const lightGrey = "#c7c7c7";
+    const [addCategoryButtonColor, setAddCategoryButtonColor] =
+        React.useState(lightGrey);
+    const [categories, setCategories] = React.useState(
+        localStorage.getItem("categories").split(",")
+    );
     const [newCategory, setNewCategory] = React.useState("");
     const [inAddCategoryMode, setInAddCategoryMode] = React.useState(false);
-    const [editState, setEditState] = React.useState({ inEditMode: false, index: 0 });
+    const [editState, setEditState] = React.useState({
+        inEditMode: false,
+        index: 0,
+    });
     const [editedCategoryText, setEditedCategoryText] = React.useState("");
     const [selectedRow, setSelectedRow] = React.useState(-1);
     const anchor = "left";
@@ -41,7 +47,7 @@ export default function SideDrawer({ open, setOpen, setSharedCategoryState }) {
     };
 
     const handleAddCategory = (event) => {
-        if (event.key === 'Enter') {
+        if (event.key === "Enter") {
             addCategory();
         }
     };
@@ -59,22 +65,23 @@ export default function SideDrawer({ open, setOpen, setSharedCategoryState }) {
     const addCategory = () => {
         const userId = localStorage.getItem("userId");
         const username = localStorage.getItem("username");
-        
+
         if (newCategory != "") {
             categories.push(newCategory);
 
-            putUser(userId, username, categories).then(res => res.data)
-            .then(user => {
-                localStorage.setItem("categories", user.categories);
-                setUserCategories(user.categories);
-                setInAddCategoryModeHelper(false);
-            })
-            .catch(err => console.log(err));
+            putUser(userId, username, categories)
+                .then((res) => res.data)
+                .then((user) => {
+                    localStorage.setItem("categories", user.categories);
+                    setUserCategories(user.categories);
+                    setInAddCategoryModeHelper(false);
+                })
+                .catch((err) => console.log(err));
         }
     };
 
     const handleEditCategory = (event, index) => {
-        if (event.key === 'Enter') {
+        if (event.key === "Enter") {
             editCategory(index);
         }
     };
@@ -85,20 +92,24 @@ export default function SideDrawer({ open, setOpen, setSharedCategoryState }) {
         const categories = localStorage.getItem("categories").split(",");
         categories[index] = editedCategoryText;
 
-        putUser(userId, username, categories).then(res => res.data)
-        .then(user => {
-            setUserCategories(user.categories);
-            setEditedCategoryText("");
-            setEditStateHelper({ inEditMode: false, index: 0 });
-        })
-        .catch(err => console.log(err));
+        putUser(userId, username, categories)
+            .then((res) => res.data)
+            .then((user) => {
+                setUserCategories(user.categories);
+                setEditedCategoryText("");
+                setEditStateHelper({ inEditMode: false, index: 0 });
+            })
+            .catch((err) => console.log(err));
     };
 
     const toggleDrawer = (isOpen) => (event) => {
-        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+        if (
+            event.type === "keydown" &&
+            (event.key === "Tab" || event.key === "Shift")
+        ) {
             return;
         }
-    
+
         setOpen(isOpen);
         setNewCategory("");
         setInAddCategoryModeHelper(false);
@@ -126,62 +137,77 @@ export default function SideDrawer({ open, setOpen, setSharedCategoryState }) {
     const AddCategoryButton = () => {
         if (!inAddCategoryMode) {
             return (
-                <Box sx={{ marginLeft: "30px" }} 
+                <Box
+                    sx={{ marginLeft: "30px" }}
                     onMouseEnter={() => handleAddButtonHighlight(true)}
                     onMouseLeave={() => handleAddButtonHighlight(false)}
                 >
                     <Grid container>
                         <Grid item sx={styles.dividerContainer}>
-                            <Divider color={addCategoryButtonColor} sx={styles.divider} />
+                            <Divider
+                                color={addCategoryButtonColor}
+                                sx={styles.divider}
+                            />
                         </Grid>
                         <Grid item sx={{ margin: 0, padding: 0 }}>
                             <IconButton
                                 onClick={handleAddCategoryBtnClicked}
-                                sx={{fontSize: "1rem", padding: 0}}
+                                sx={{ fontSize: "1rem", padding: 0 }}
                             >
-                                <AddBoxIcon sx={{ fontSize: "1.7em", color: addCategoryButtonColor }}/>
+                                <AddBoxIcon
+                                    sx={{
+                                        fontSize: "1.7em",
+                                        color: addCategoryButtonColor,
+                                    }}
+                                />
                             </IconButton>
                         </Grid>
                         <Grid item sx={styles.dividerContainer}>
-                            <Divider color={addCategoryButtonColor} sx={styles.divider} />
+                            <Divider
+                                color={addCategoryButtonColor}
+                                sx={styles.divider}
+                            />
                         </Grid>
                     </Grid>
                 </Box>
-            )
+            );
         }
     };
 
     const drawerItems = () => (
-        <Box
-            sx={{ width: 250 }}
-            role="presentation"
-        >
-            <Typography sx={{ margin: "20px 0 10px 10px", fontSize: "1.5rem" }}>Categories</Typography>
-            <Divider color={lightGrey}/>
+        <Box sx={{ width: 250 }} role="presentation">
+            <Typography sx={{ margin: "20px 0 10px 10px", fontSize: "1.5rem" }}>
+                Categories
+            </Typography>
+            <Divider color={lightGrey} />
             <List>
-                {categories.map((text, index) => (
-                    editState.inEditMode && editState.index === index ?
-                    <TextField
-                        onChange={(e) => setEditedCategoryText(e.target.value)}
-                        onKeyDown={(e) => handleEditCategory(e, index)}
-                        id="outlined-basic"
-                        variant="filled"
-                        defaultValue={text}
-                        sx={{ marginLeft: "5px" }}
-                    /> :
-                    <CategoryRow
-                        key={`${index}-${text}`}
-                        text={text}
-                        index={index}
-                        selectedRow={selectedRow}
-                        setUserCategories={setUserCategories}
-                        setEditStateHelper={setEditStateHelper}
-                        handleCategorySelected={handleCategorySelected}
-                        setSharedCategoryState={setSharedCategoryState}
-                    />
-                ))}
+                {categories.map((text, index) =>
+                    editState.inEditMode && editState.index === index ? (
+                        <TextField
+                            onChange={(e) =>
+                                setEditedCategoryText(e.target.value)
+                            }
+                            onKeyDown={(e) => handleEditCategory(e, index)}
+                            id="outlined-basic"
+                            variant="filled"
+                            defaultValue={text}
+                            sx={{ marginLeft: "5px" }}
+                        />
+                    ) : (
+                        <CategoryRow
+                            key={`${index}-${text}`}
+                            text={text}
+                            index={index}
+                            selectedRow={selectedRow}
+                            setUserCategories={setUserCategories}
+                            setEditStateHelper={setEditStateHelper}
+                            handleCategorySelected={handleCategorySelected}
+                            setSharedCategoryState={setSharedCategoryState}
+                        />
+                    )
+                )}
             </List>
-            {inAddCategoryMode && 
+            {inAddCategoryMode && (
                 <TextField
                     onChange={(e) => setNewCategory(e.target.value)}
                     onKeyDown={handleAddCategory}
@@ -190,20 +216,16 @@ export default function SideDrawer({ open, setOpen, setSharedCategoryState }) {
                     variant="filled"
                     sx={{ marginLeft: "10px" }}
                 />
-            }
+            )}
             <AddCategoryButton />
         </Box>
     );
 
     return (
         <div>
-            <Drawer
-                anchor={anchor}
-                open={open}
-                onClose={toggleDrawer(false)}
-            >
+            <Drawer anchor={anchor} open={open} onClose={toggleDrawer(false)}>
                 {drawerItems()}
             </Drawer>
         </div>
-    )
+    );
 }
--- src/services/usersApi.js
+++ src/services/usersApi.js
@@ -1,10 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseUrl = "http://localhost:8080/api/users"
+const baseUrl = "http://localhost:8080/api/users";
 
 export function getUser() {
     const userId = localStorage.getItem("userId");
-    
+
     try {
         const response = axios.get(`${baseUrl}?id=${userId}`);
         return response;
@@ -18,10 +18,10 @@ export function putUser(id, username, categories) {
         const response = axios.put(baseUrl, {
             id: id,
             username: username,
-            categories: categories
+            categories: categories,
         });
         return response;
     } catch (error) {
-        return error; 
+        return error;
     }
 }
